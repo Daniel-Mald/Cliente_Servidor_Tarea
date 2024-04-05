513cd60748115a5ebfbf3e18a3aa91f9b74ccefe
Carlos Daniel Maldonado Muñoz <201G0266@rcarbonifera.tecnm.mx>
2024-04-04 18:48:29 -0600

    Ya parpadea y tiene sonidos

--- VMS/ViewModels/VmsViewModel.cs
+++ VMS/ViewModels/VmsViewModel.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using VMS.Models.DTOs;
 using VMS.Services;
+using System.Timers;
+using System.Media;
 
 namespace VMS.ViewModels
 {
@@ -15,6 +17,11 @@ namespace VMS.ViewModels
     {
         public ObservableCollection<VmsDTO> _vmsLista { get; set; } = new ObservableCollection<VmsDTO>();
         VmsService _server;
+        System.Timers.Timer _timer;
+        Random _random;
+        SoundPlayer _player;
+        SoundPlayer _player2;
+        SoundPlayer _player3;
         int _numCarteles;
         private readonly Dictionary<string, string[]> coloresReales = new()
 {
@@ -24,6 +31,7 @@ namespace VMS.ViewModels
     { "Naranja", new string[] { "#FFA500", "#FFE2C6" }},
     { "Cian", new string[] { "#00FFFF", "#E2FFFF" }},
     { "Magenta", new string[] { "#FF2EFF", "#FFE2FF" }}
+         ,{"Apagado", new string [] {"Black","Transparent"}}
 };
 
         public string IP 
@@ -41,7 +49,57 @@ namespace VMS.ViewModels
             _server.VmsRecibido += _server_VmsRecibido;
             _server.IniciarServer();
             LlenarCarteles();
+            _timer = new(5000);
+            _player = new SoundPlayer("Assets/soundLight.wav");
+            _player2 = new SoundPlayer("Assets/soundLight2.wav");
+            _player3 = new SoundPlayer("Assets/back.wav");
+
+           
+            _random = new Random();
+            _timer.Elapsed += _timer_ElapsedAsync;
+            _timer.Start();
+           
         }
+
+        private async void _timer_ElapsedAsync(object? sender, ElapsedEventArgs e)
+        {
+            _player3.Play();
+            int cartel = _random.Next(0, 16);
+            var cart = _vmsLista[cartel];
+            string[] s = new string[] { cart.Color ,cart.ColorClaro };
+
+            int ii = _random.Next(1, 50);
+            int delay = _random.Next(50, 400);
+            if (ii % 2 == 0)
+            {
+                _player.Play();
+            }
+            else
+            {
+                _player2.Play();
+            }
+            for (int i = 0; i < ii; i++)
+            {
+                _vmsLista[cartel].ColorClaro = coloresReales["Apagado"][1];
+                _vmsLista[cartel].Color = coloresReales["Apagado"][0];
+                
+                OnPropertyChanged(nameof(_vmsLista));
+                
+                
+                await Task.Delay(delay);
+
+                _vmsLista[cartel].ColorClaro = s[1];
+                _vmsLista[cartel].Color = s[0];
+                OnPropertyChanged(nameof(_vmsLista));
+                await Task.Delay(delay);
+
+            }
+           //_player.Stop();
+            //_player2.Stop();
+            
+        }
+
+        
 
         private void _server_VmsRecibido(object? sender, VmsDTO e)
         {
